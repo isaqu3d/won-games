a6359349968844bc0ff723841798e349f7c074c3
Isaque de Sousa <isaque.error404@gmail.com>
2024-09-21 11:42:22 -0300

    feat: add new config jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,21 @@
-module.exports = {
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './'
+})
+
+const customJestConfig = {
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1'
+  },
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   }
 }
+
+export default createJestConfig(customJestConfig)
