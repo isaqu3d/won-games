0a1db0d8729da7fdb4c8d3bf08da5e08c4d6274e
Isaque de Sousa <isaque.error404@gmail.com>
2024-10-07 23:13:26 -0300

    feat: add styles

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,5 +1,5 @@
 const Main = () => (
-  <main>
+  <main className="bg-[#06092b] text-white w-full h-full p-12 flex items-center justify-center text-center flex-col">
     <h1>Won games</h1>
   </main>
 )
