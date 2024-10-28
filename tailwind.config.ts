112cc733a63ae5cdc4770aa30556f67afc05e192
Isaque de Sousa <isaque.error404@gmail.com>
2024-10-28 14:17:40 -0300

    feat: add new fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          '"Open Sans"',
+          '"Helvetica Neue"',
+          'sans-serif'
+        ]
+      },
+      fontSize: {
+        base: '10px'
       }
     }
   },
