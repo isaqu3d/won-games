0881333dba4d4500c7a7044bffa6dc8b3b94ee77
Isaque de Sousa <isaque.error404@gmail.com>
2024-10-30 10:59:38 -0300

    chore: test story book props

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,6 +1,25 @@
-const Main = () => (
-  <main className="bg-[#06092b] text-white w-full h-full p-12 flex items-center justify-center text-center flex-col">
-    <h1>Won games</h1>
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e TailwindCSS'
+}: {
+  title: string
+  description: string
+}) => (
+  <main className="bg-[#06092b] text-white text-lg h-full font-sans p-12 flex items-center justify-center text-center flex-col">
+    <img
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+      className="w-80 mb-8"
+    />
+
+    <h1>{title}</h1>
+    <p>{description}</p>
+
+    <img
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+      className="w-full max-w-[30rem] mt-12"
+    />
   </main>
 )
 
