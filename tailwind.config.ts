7d633925b027da00e238624a4a89f9b806edf2d9
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-09 11:45:06 -0300

    chore: add colors e spacings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,8 +23,34 @@ const config: Config = {
           'sans-serif'
         ]
       },
-      fontSize: {
-        base: '10px'
+
+      colors: {
+        primary: '#F231A5',
+        secondary: '#3CD3C1',
+        mainbg: '#06092B',
+        white: '#FAFAFA',
+        black: '#030517',
+        lightgray: '#EAEAEA',
+        gray: '#8F8F8F',
+        darkgray: '#2E2F42'
+      },
+
+      spacing: {
+        xxsmall: '0.8rem',
+        xsmall: '1.6rem',
+        small: '2.4rem',
+        medium: '3.2rem',
+        large: '4.0rem',
+        xlarge: '4.8rem',
+        xxlarge: '5.6rem'
+      },
+
+      zIndex: {
+        base: '10',
+        menu: '20',
+        overlay: '30',
+        modal: '40',
+        alwaysOnTop: '50'
       }
     }
   },
