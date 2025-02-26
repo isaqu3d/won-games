23d8fd8da59a180a1abce07499f9bd1dd32ec283
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-26 15:51:39 -0300

    chore: add test color black

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -2,11 +2,19 @@ import { render, screen } from '@testing-library/react'
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render a white label by default ', () => {
+  it('should render a white label by default', () => {
     render(<Logo />)
 
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveClass(
       'text-white'
+    )
+  })
+
+  it('should render a black label when color is passed', () => {
+    render(<Logo color="black" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveClass(
+      'text-black'
     )
   })
 })
