e23433043cdf21937aec82612fa25235d8a1a025
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-26 17:02:47 -0300

    refactor: add arg types

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,9 +1,40 @@
-import { Meta, StoryObj } from '@storybook/react'
-import Logo from '.'
+import { Meta, StoryFn } from '@storybook/react'
+import Logo, { LogoProps } from '.'
 
 export default {
   title: 'Logo',
-  component: Logo
+  component: Logo,
+  args: {
+    color: 'black',
+    size: 'md',
+    hideOnMobile: false
+  },
+  argTypes: {
+    color: {
+      options: ['white', 'black'],
+      control: {
+        type: 'inline-radio'
+      }
+    },
+    size: {
+      options: ['md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
+  }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryFn<LogoProps> = (args) => <Logo {...args} />
+
+export const Large: StoryFn<LogoProps> = (args) => <Logo {...args} />
+
+Large.args = {
+  size: 'md'
+}
+
+export const Mobile: StoryFn<LogoProps> = (args) => <Logo {...args} />
+
+Mobile.args = {
+  hideOnMobile: true
+}
