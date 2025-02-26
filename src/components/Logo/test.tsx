20f7cedcfc768725eee6da603adb99bab28d25e5
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-26 01:16:59 -0300

    feat: add test render a white label by default

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -2,9 +2,11 @@ import { render, screen } from '@testing-library/react'
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a white label by default ', () => {
+    render(<Logo />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveClass(
+      'text-white'
+    )
   })
 })
