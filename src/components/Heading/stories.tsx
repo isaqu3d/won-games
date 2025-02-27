230da9b6160109acf2736d6e91a112de3838a23a
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-27 09:47:59 -0300

    chore: add arg types

--- src/components/Heading/stories.tsx
+++ src/components/Heading/stories.tsx
@@ -1,9 +1,18 @@
 import { Meta, StoryObj } from '@storybook/react'
-import Heading from '.'
+import Heading, { HeadingProps } from '.'
 
 export default {
   title: 'Heading',
-  component: Heading
+  component: Heading,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<HeadingProps> = {}
+
+Default.args = {
+  children: 'Most Popular'
+}
