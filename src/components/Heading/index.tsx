2f6b5910f1736ae763b3a238ef8fc9dba10489b9
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-28 13:23:08 -0300

    feat(Heading): added support for color, lineLeft, and lineBottom prop.

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,10 +1,32 @@
-import { ReactNode } from 'react'
+import clsx from 'clsx'
+import React from 'react'
 
 export type HeadingProps = {
-  children: ReactNode
+  children: React.ReactNode
+  color?: 'white' | 'black'
+  lineLeft?: boolean
+  lineBottom?: boolean
 }
 
-const Heading = ({ children }: HeadingProps) => (
-  <h2 className="font-sans font-semibold sm:text-3xl text-2xl">{children}</h2>
-)
+const Heading = ({
+  children,
+  color = 'white',
+  lineLeft = false,
+  lineBottom = false
+}: HeadingProps) => {
+  return (
+    <h2
+      className={clsx(
+        'text-2xl md:text-3xl font-semibold font-sans',
+        color === 'white' ? 'text-white' : 'text-black',
+        lineLeft && 'pl-2 border-l-8 border-secondary',
+        lineBottom &&
+          'relative mb-4 after:absolute after:left-0 after:-bottom-4 after:w-20 after:border-4 after:border-primary'
+      )}
+    >
+      {children}
+    </h2>
+  )
+}
+
 export default Heading
