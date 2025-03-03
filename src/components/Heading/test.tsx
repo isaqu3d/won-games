b66368c650a213fed3044d3fd97e48e5e8b66330
Isaque de Sousa <isaque.error404@gmail.com>
2025-03-03 10:58:02 -0300

    feat: add test heading

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -2,11 +2,35 @@ import { render, screen } from '@testing-library/react'
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render a white label by default', () => {
-    render(<Heading>Won Games</Heading>)
+  it('should render a white heading by default', () => {
+    render(<Heading>Most Popular</Heading>)
 
-    expect(screen.getByRole('heading', { name: /won games/i })).toHaveClass(
+    expect(screen.getByRole('heading', { name: /most popular/i })).toHaveClass(
       'text-white'
+    )
+  })
+
+  it('should render a black heading when is color passed', () => {
+    render(<Heading color="black">Most Popular</Heading>)
+
+    expect(screen.getByRole('heading', { name: /most popular/i })).toHaveClass(
+      'text-black'
+    )
+  })
+
+  it('should render a heading with a line to the left side', () => {
+    render(<Heading lineLeft>Most Popular</Heading>)
+
+    expect(screen.getByRole('heading', { name: /most popular/i })).toHaveClass(
+      'pl-2 border-l-8 border-secondary'
+    )
+  })
+
+  it('should render a heading with a line at the bottom', () => {
+    render(<Heading lineBottom>Most Popular</Heading>)
+
+    expect(screen.getByRole('heading', { name: /most popular/i })).toHaveClass(
+      'relative mb-4 after:absolute after:left-0 after:-bottom-4 after:w-20 after:border-4 after:border-primary'
     )
   })
 })
