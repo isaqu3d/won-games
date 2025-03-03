964ccf0f35bc95ad989b3c432c26bdbcf2f9ec2d
Isaque de Sousa <isaque.error404@gmail.com>
2025-03-03 10:38:46 -0300

    feat: add test heading

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -2,11 +2,11 @@ import { render, screen } from '@testing-library/react'
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Heading />)
-    expect(
-      screen.getByRole('heading', { name: /Heading/i })
-    ).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a white label by default', () => {
+    render(<Heading>Won Games</Heading>)
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveClass(
+      'text-white'
+    )
   })
 })
