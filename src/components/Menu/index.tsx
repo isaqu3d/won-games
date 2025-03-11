4d4076890f1fc9553e2ca51fc2e7df46527cd988
Isaque de Sousa <isaque.error404@gmail.com>
2025-03-10 21:30:52 -0300

    chore: menu component to include icons and logo

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,23 @@
+import {
+  AlignLeft as MenuIcon,
+  Search as SearchIcon,
+  ShoppingCart as ShoppingCartIcon
+} from 'lucide-react'
+import Logo from '../Logo'
+
 const Menu = () => (
-  <main>
-    <h1>Menu</h1>
-  </main>
+  <menu className="flex text-white items-center py-4 relative">
+    <MenuIcon />
+
+    <div className="absolute left-1/2 transform -translate-x-1/2 md:left-auto md:translate-x-0">
+      <Logo hideOnMobile />
+    </div>
+
+    <div className="flex flex-grow justify-end gap-2">
+      <SearchIcon />
+
+      <ShoppingCartIcon />
+    </div>
+  </menu>
 )
 export default Menu
